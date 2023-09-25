a178b8c5182554043836a57a557c4345e4d67c0e
Andrea Telatin, M1 <15690844+telatin@users.noreply.github.com>
2023-09-25 10:26:15 +0100

    pre-release

--- test.pl
+++
@@ -1,8 +0,0 @@
-use FASTX::Reader;
-my $nucleotidic_seq = FASTX::Seq->new(
-            -seq => 'ATGATGGGGTTT', #$f->seq->seq,
-            -id => 'cool', #s('ID'))[0],
-      );
-
-my $orf =  $nucleotidic_seq->translate(11);
-print $orf->seq;
